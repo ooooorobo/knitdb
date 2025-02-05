30f54a14fb4f21efc08ad82f1cdd0a2c9c460ec3
조예진 <orobos654@gmail.com>
2025-02-06 01:16:21 +0900

    feat: add header

--- src/app/root.tsx
+++ src/app/root.tsx
@@ -1,7 +1,29 @@
-import { Links, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
-import { LinksFunction } from '@remix-run/node';
+import {
+  Link,
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from '@remix-run/react';
+import { LinksFunction, LoaderFunction, redirect } from '@remix-run/node';
+import { createSupabaseClient } from 'src/libs/supabase/createClient.server';
 
-export const links: LinksFunction = () => [{ rel: 'stylesheet', href: 'https://matcha.mizu.sh/matcha.css' }];
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: 'https://matcha.mizu.sh/matcha.css' },
+];
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const { client, headers } = createSupabaseClient(request);
+
+  const session = client.auth.getSession();
+
+  if (!session) {
+    throw redirect('/login', { headers });
+  }
+
+  return new Response('', { headers });
+};
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -13,6 +35,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
+        <header>
+          <Link to={'/'}>뜨개디비</Link>
+          <Link to={'/item/new'}>추가</Link>
+          <Link to={'/logout'}>로그아웃</Link>
+        </header>
         {children}
         <ScrollRestoration />
         <Scripts />
